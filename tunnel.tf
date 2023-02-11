cbf7210cb40a2d4e15a9c6ac98682989f68eddf3
Jared <jprest97@gmail.com>
2023-02-11 17:02:28 +0000

    Moved CNAME records into a separate file

--- tunnel.tf
+++ tunnel.tf
@@ -30,15 +30,3 @@ resource "cloudflare_tunnel_config" "config" {
 
   }
 }
-
-resource "cloudflare_record" "dns" {
-  for_each = {for index, ingress_rule in local.ingress_rules:
-    ingress_rule.subdomain => ingress_rule
-  }
-  name     = each.value.subdomain
-  proxied  = true
-  ttl      = 1
-  type     = "CNAME"
-  value    = "${cloudflare_argo_tunnel.tunnel.id}.cfargotunnel.com"
-  zone_id  = data.cloudflare_zone.zone.id
-}
