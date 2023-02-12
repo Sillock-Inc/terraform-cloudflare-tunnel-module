03510df47c5180cf6929d540520315381d5da40e
Jared <jprest97@gmail.com>
2023-02-12 21:02:23 +0000

    added default values for variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "cloudflare_account_id" {
   type        = string
   description = "Account ID for the Cloudflare account to provision resources under"
+  default     = ""
 }
 
 variable "tunnel_name" {
   type        = string
   description = "DNS name for the tunnel e.g. `example.com`"
+  default     = ""
 }
 
 variable "ingress_rules" {
@@ -14,9 +16,15 @@ variable "ingress_rules" {
     address   = string
   }))
   description = "Subdomain name and kubernetes KubeDNS name as a key value pair for Cloudflare tunnel config"
+  
+  default = [ {
+    address = ""
+    subdomain = ""
+  } ]
 }
 
 variable "zone_dns" {
   type        = string
   description = "DNS name for the tunnel e.g. `example.com`"
+  default     = ""
 }
